a33d967ab735e321725a5c577813ee72bd9eff82
bigcountry-scav <44387996+bigcountry-scav@users.noreply.github.com>
2021-09-09 12:30:36 -0400

    Update updateEndpoints.sh

--- updateEndpoints.sh
+++ updateEndpoints.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+if ! [ $TRAVIS_BRANCH =~ ^(dev|beta)$ ]; then
+  endpoint=${TRAVIS_REPO_SLUG//ScavengerBot\/scavenger-/}
+fi
+  
+if [ $endpoint ]; then
+  curl -X PUT -H "Content-Type: application/json" -d {} https://api-dev.scavengerbot.io/v1/admin/services
+fi
